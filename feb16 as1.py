ef382f27592e0901128d0e0e723c8ad8db17617f
lateef3129 <99283767+lateef3129@users.noreply.github.com>
2022-02-17 11:18:08 +0530

    Add files via upload

---
+++ feb16 as1.py
@@ -0,0 +1,3 @@
+n=int(input('enter n value'))
+for n in range (0,n) :
+    print(n,n*n)
---
+++ feb16 ass2.py
@@ -0,0 +1,8 @@
+year = int(input("enter the year"))
+if (year % 400 == 0) and (year % 100 == 0):
+    print("true")
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("true")
+else:
+    print("false")
+    
